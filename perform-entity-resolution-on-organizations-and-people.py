419342f4cd9384156d8106b1f26e5b5677353c8e
Cleisson Flores <cleissongremista@hotmail.com>
2023-12-05 10:50:07 -0300

    evaluate prefect batch submitjob

--- perform-entity-resolution-on-organizations-and-people.py
+++ perform-entity-resolution-on-organizations-and-people.py
@@ -1,34 +1,42 @@
 import time
 from prefect import flow, task
+from prefect_aws.batch import batch_submit
 from datetime import timedelta
 
 import boto3
 
 @task
 def batch_submit_job(job_name: str, job_queue: str, job_definition: str, env: dict):
-    batch = boto3.client('batch', region_name='us-east-2')
-    response = batch.submit_job(
-        jobName=job_name,
-        jobQueue=job_queue,
-        jobDefinition=job_definition,
+    #batch = boto3.client('batch', region_name='us-east-2')
+    job_id = batch_submit(
+        job_name=job_name,
+        job_definition=job_definition,
+        job_queue=job_queue,
+        region_name='us-east-2',
         containerOverrides={'environment': [{'name': k, 'value': v} for k, v in env.items()]}
     )
+    # response = batch.submit_job(
+    #     jobName=job_name,
+    #     jobQueue=job_queue,
+    #     jobDefinition=job_definition,
+    #     containerOverrides={'environment': [{'name': k, 'value': v} for k, v in env.items()]}
+    # )
     
     # Get JobId from response
-    job_id = response['jobId']
+    #job_id = response['jobId']
 
     # Poll job status
-    while True:
-        response = batch.describe_jobs(jobs=[job_id])
-        status = response['jobs'][0]['status']
+    # while True:
+    #     response = batch.describe_jobs(jobs=[job_id])
+    #     status = response['jobs'][0]['status']
 
-        if status == 'SUCCEEDED':
-            break
-        elif status == 'FAILED':
-            raise Exception(f'Job {job_id} failed')
-        else:
-            print(f"Job {job_id} is still in {status} status, waiting...")
-            time.sleep(120) # Wait a bit before polling again
+    #     if status == 'SUCCEEDED':
+    #         break
+    #     elif status == 'FAILED':
+    #         raise Exception(f'Job {job_id} failed')
+    #     else:
+    #         print(f"Job {job_id} is still in {status} status, waiting...")
+    #         time.sleep(120) # Wait a bit before polling again
 
 @task
 def run_flow():
