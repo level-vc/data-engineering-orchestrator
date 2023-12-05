e221f4cee46b493bc719e206180e09e85773b77a
Cleisson Flores <cleissongremista@hotmail.com>
2023-12-05 11:18:43 -0300

    added new batch submit job implementation

--- perform-entity-resolution-on-organizations-and-people.py
+++ perform-entity-resolution-on-organizations-and-people.py
@@ -1,48 +1,39 @@
 import time
-import os
-os.environ['AWS_REGION'] = 'us-east-2'
 from prefect import flow, task
-from prefect_aws import AwsCredentials
-from prefect_aws.batch import batch_submit
 from datetime import timedelta
 
 import boto3
 
 
-def batch_submit_job(job_name: str, job_queue: str, job_definition: str, env: dict):
-    #batch = boto3.client('batch', region_name='us-east-2')
-    aws_credentials = AwsCredentials(
-        region_name='us-east-2'
-    )
-    job_id = batch_submit(
-        job_name=job_name,
-        job_definition=job_definition,
-        job_queue=job_queue,
-        aws_credentials=aws_credentials,
-        containerOverrides={'environment': [{'name': k, 'value': v} for k, v in env.items()]}
-    )
-    # response = batch.submit_job(
-    #     jobName=job_name,
-    #     jobQueue=job_queue,
-    #     jobDefinition=job_definition,
-    #     containerOverrides={'environment': [{'name': k, 'value': v} for k, v in env.items()]}
-    # )
-    
-    # Get JobId from response
-    #job_id = response['jobId']
+@task
+async def batch_submit(
+    job_name: str,
+    job_queue: str,
+    job_definition: str,
+    region_name='us-east-2',
+    **batch_kwargs,
+) -> str:
+    """
+    ...
+    """
+    print("Preparing to submit %s job to %s job queue", job_name, job_queue)
 
-    # Poll job status
-    # while True:
-    #     response = batch.describe_jobs(jobs=[job_id])
-    #     status = response['jobs'][0]['status']
+    batch_client = boto3.client("batch", region_name=region_name)
 
-    #     if status == 'SUCCEEDED':
-    #         break
-    #     elif status == 'FAILED':
-    #         raise Exception(f'Job {job_id} failed')
-    #     else:
-    #         print(f"Job {job_id} is still in {status} status, waiting...")
-    #         time.sleep(120) # Wait a bit before polling again
+    response = batch_client.submit_job(
+        jobName=job_name,
+        jobQueue=job_queue,
+        jobDefinition=job_definition,
+        **batch_kwargs,
+    )
+
+    job_id = response["jobId"]
+
+    # Wait for the job to complete
+    waiter = batch_client.get_waiter('job_execution_complete')
+    waiter.wait(jobs=[job_id])
+
+    return job_id
 
 @task
 def run_flow():
@@ -54,7 +45,7 @@ github_branch = 'main'
 
 @flow(log_prints=True)
 def perform_entity_resolution_on_organizations_people(name: str = "world"):
-    batch_submit_job(
+    batch_submit(
         job_name="copy-from-rds-to-s3",
         job_definition="arn:aws:batch:us-east-2:058442094236:job-definition/copy-overwrites-RDS-tables-to-Athena",
         job_queue="arn:aws:batch:us-east-2:058442094236:job-queue/etl-queue",
@@ -63,14 +54,14 @@ def perform_entity_resolution_on_organizations_people(name: str = "world"):
 
     run_flow()
 
-    batch_submit_job(
+    batch_submit(
         job_name="er-people",
         job_definition="arn:aws:batch:us-east-2:058442094236:job-definition/er_people_match_entities",
         job_queue="arn:aws:batch:us-east-2:058442094236:job-queue/etl-queue",
         env={ "ENV": env, "GITHUB_BRANCH": github_branch }
     )
 
-    batch_submit_job(
+    batch_submit(
         job_name="copy-from-rds-to-s3",
         job_definition="arn:aws:batch:us-east-2:058442094236:job-definition/copy-overwrites-RDS-tables-to-Athena",
         job_queue="arn:aws:batch:us-east-2:058442094236:job-queue/etl-queue",
